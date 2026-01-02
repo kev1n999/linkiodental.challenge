af01ba73bf8333eca93582b5fbbb8de3ea08d367
kev1n999 <sopratesta999@gmail.com>
2026-01-02 16:45:01 -0300

    fix: add mongo-connect function to async

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-export function connect(mongoUri: string) {
+export async function connect(mongoUri: string) {
   try {
-    mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri);
     console.log("You are connected on mongodb!");
   } catch (err) {
     console.error(`An error occurred while connecting to mongodb\n\n${err}`);
--- app/index.ts
+++ app/index.ts
@@ -3,8 +3,11 @@ import app from "./server.js";
 import { env } from "./config/env.js";
 import { connect } from "./config/database.js";
 
-connect(env.mongouri);
+async function bootstrap() {
+  await connect(env.mongouri);
+  app.listen(env.serverport, () => {
+    console.log(`Server listening on http://localhost:${env.serverport}`);
+  });
+}
 
-app.listen(env.serverport, () => {
-  console.log(`Server listening on http://localhost:${env.serverport}`);
-});
+bootstrap();
